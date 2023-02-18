154751526ca2b8ea9dad31a9ff742b8ce8ee9299
Pedro Monteiro <2190769@my.ipleiria.pt>
2023-02-18 00:11:02 +0000

    Security Reasons
    Security

--- siteTournamentOverView/settings.py
+++ siteTournamentOverView/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)_t%8tupogoeef*^-bfv(*41jjtzcq10t93dzc^hrr0^wv6kd$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
